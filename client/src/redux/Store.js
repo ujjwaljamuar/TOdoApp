22be10e6d73d917c7038eb25ea2ca29b4a73572d
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2023-04-26 11:56:32 +0530

    changes in store

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -3,12 +3,16 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import todosReducers from "./reducers/todoReducers";
+
+// create all the reducers you want in a json format
 const reducer = combineReducers({
-  todos: todosReducers
+  todos: todosReducers,
 });
 
+// middleware
 const middleware = [thunk];
 
+// create store using redux
 const Store = createStore(
   reducer,
   composeWithDevTools(applyMiddleware(...middleware))
